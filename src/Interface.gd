7340355a4c49af641658378b332798bafbfe7d29
Anthony Wilcox <35226681+tomiscw@users.noreply.github.com>
2018-12-30 07:39:01 -0500

    Bind Screenshot and About buttons

--- src/Interface.gd
+++ src/Interface.gd
@@ -1,15 +1,7 @@
 extends Control
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
-func _ready():
-	# Called when the node is added to the scene for the first time.
-	# Initialization here
+func _on_ScreenshotBtn_pressed():
 	pass
 
-#func _process(delta):
-#	# Called every frame. Delta is time since last frame.
-#	# Update game logic here.
-#	pass
+func _on_AboutBtn_pressed():
+	pass

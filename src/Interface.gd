2d2ecebd28d20cdfc3a08d90f53069055b5af1ab
Anthony Wilcox <35226681+tomiscw@users.noreply.github.com>
2018-12-31 07:57:22 -0500

    Removed Screenshot button and binding function

--- src/Interface.gd
+++ src/Interface.gd
@@ -6,10 +6,6 @@ onready var coming_soon_win = preload("res://windows/ComingSoon.tscn")
 onready var about_win = preload("res://windows/About.tscn")
 onready var credits_win = preload("res://windows/Credits.tscn")
 
-func _on_ScreenshotBtn_pressed():
-	var win_instance = coming_soon_win.instance()
-	add_child(win_instance)
-
 func _on_AboutBtn_pressed():
 	var win_instance = about_win.instance()
 	add_child(win_instance)

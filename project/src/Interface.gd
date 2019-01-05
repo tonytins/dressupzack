b85ee22101261a4298a92ff241ae7e7364f9fae7
Anthony Wilcox <35226681+tomiscw@users.noreply.github.com>
2019-01-05 09:17:59 -0500

    Start screen
    - Created a start screen with the game title in order to switch between Modern and Classic modes. I moved the credits and license buttons out of the About window and into this screen. The About window itself is gona now since it kinda redundant, anyway.

--- project/src/Interface.gd
+++ project/src/Interface.gd
@@ -2,14 +2,8 @@
 # See the LICENSE file in the project root for more information.
 extends Control
  
-func _on_AboutBtn_pressed():
-	$WinDialogs/AboutWin.show()
-	
 func _on_SettingsBtn_pressed():
 	$WinDialogs/SettingsWin.show()
 
-func _on_ClassicBtn_pressed():
-	get_tree().change_scene("res://ClassicScreen.tscn")
-
-func _on_ModernBtn_pressed():
-	get_tree().change_scene("res://GameScreen.tscn")
+func _on_ExitBtn_pressed():
+	get_tree().change_scene("res://StartScn.tscn")
---
+++ project/src/StartScn.gd
@@ -0,0 +1,31 @@
+# Anthony Wilcox licenses this file to you under the GPL license.
+# See the LICENSE file in the project root for more information.
+extends Node
+
+func _on_ClassicBtn_pressed():
+	get_tree().change_scene("res://ClassicScn.tscn")
+
+func _on_ModernBtn_pressed():
+	get_tree().change_scene("res://GameScn.tscn")
+
+func _on_ClassicBtn_mouse_entered():
+	$ModernBG.hide()
+	$WordrobeBG.hide()
+	$ClassicBG.show()
+
+func _on_ClassicBtn_mouse_exited():
+	$ClassicBG.hide()
+	$WordrobeBG.show()
+	$ModernBG.show()
+
+func _on_CreditsBtn_pressed():
+	$WinDialogs/CreditsWin.show()
+
+func _on_LicenseBtn_pressed():
+	$WinDialogs/LicenseWin.show()
+
+func _on_PlayBtn_toggled(button_pressed):
+	if button_pressed == true:
+		$MenuRf/ModePanel.show()
+	else:
+		$MenuRf/ModePanel.hide()

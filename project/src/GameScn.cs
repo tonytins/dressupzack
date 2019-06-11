20da3fde06b01aa40f20cc3b59c1c4b8f9cfa494
Anthony Wilcox <35226681+antonwilc0x@users.noreply.github.com>
2019-06-10 21:38:57 -0400

    Further streamlined functionality

--- project/src/GameScn.cs
+++ project/src/GameScn.cs
@@ -2,8 +2,9 @@ using Godot;
 
 public enum ClothingType
 {
-    Top,
-    Bottom,
+    Shirts,
+    Pants,
+    Undies,
     Accessory
 }
 
@@ -11,17 +12,21 @@ public class GameScn : Node
 {
     void ChangeClothes(string path, ClothingType clothingType)
     {
-        var texture = ResourceLoader.Load<Texture>(path);
+        var texture = ResourceLoader.Load<Texture>($"res://{path}");
 
         switch (clothingType)
         {
-            case ClothingType.Top:
-                var top = GetNode<Sprite>("Nathan/Accessory");
+            case ClothingType.Shirts:
+                var top = GetNode<Sprite>("Nathan/Top");
                 top.Texture = texture;
                 break;
-            case ClothingType.Bottom:
-                var bottom = GetNode<Sprite>("Nathan/Accessory");
+            case ClothingType.Pants:
+                var bottom = GetNode<Sprite>("Nathan/Bottom");
                 bottom.Texture = texture;
+                break;
+            case ClothingType.Undies:
+                var undies = GetNode<Sprite>("Nathan/Undies");
+                undies.Texture = texture;
                 break;
             case ClothingType.Accessory:
                 var accessory = GetNode<Sprite>("Nathan/Accessory");
@@ -30,11 +35,26 @@ public class GameScn : Node
         }
     }
 
+    TextureButton ClothingButton(string path, ClothingType clothingType)
+    {
+        switch (clothingType)
+        {
+            case ClothingType.Accessory:
+                return GetNode<TextureButton>($"Clothes/Wordrobe/AccsGrid/{path}");
+            case ClothingType.Undies:
+                return GetNode<TextureButton>($"Clothes/Wordrobe/UndiesGrid/{path}");
+            default:
+            case ClothingType.Pants:
+                return GetNode<TextureButton>($"Clothes/Wordrobe/PantsGrid/{path}");
+            case ClothingType.Shirts:
+                return GetNode<TextureButton>($"Clothes/Wordrobe/ShirtsGrid/{path}");
+        }
+    }
+
     public override void _Process(float delta)
     {
         // Change clothes
-        if (GetNode<TextureButton>("Clothes/Wordrobe/Accessoires/AccsGrid/CanonCam").IsPressed())
-            ChangeClothes("res://sprites/camera.png", ClothingType.Accessory);
-        
+        if (ClothingButton("CanonCam", ClothingType.Accessory).IsPressed())
+            ChangeClothes("sprites/camera.png", ClothingType.Accessory);
     }
 }

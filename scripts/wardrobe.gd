0d280d61e0dd1b5e616186afcb108c8eb0026e9b
Anthony Foxclaw <noreply@tonytins.xyz>
2020-07-06 02:28:46 -0400

    Made DLC loading it's own function

--- scripts/wardrobe.gd
+++ scripts/wardrobe.gd
@@ -9,34 +9,32 @@ onready var shirts_grid = $Shirts/ShirtsScroll/ShirtsGrid
 onready var undies_grid = $Underwear/UndiesScroll/UndiesGrid
 onready var accs_grid = $Accessoires/AccsScroll/AccsGrid
 
-func _ready():
-	var dlc_pack = "user://dlc/testdlc.pck"
-	var load_pck = ProjectSettings.load_resource_pack(dlc_pack, false)
+func get_dlc(dlc_pack, scenes: Array):
+	var usr_dir = "user://dlc/"
+	var dlc_path = usr_dir + dlc_pack
+	var load_pck = ProjectSettings.load_resource_pack(dlc_path, false)
+	print_debug("Loaded " + dlc_path + ": " + str(load_pck))
+	
+	# Check if the DLC is actually there
 	if load_pck == true:
-		ProjectSettings.load_resource_pack(dlc_pack, false)
-		var dir = Directory.new()
-		if dir.open("res://scenes/dlc/shirts") == OK:
-			dir.list_dir_begin()
-			var shirts = dir.get_next()
-			while shirts != "":
-				var shirt = "res://scenes/dlc/shirts/" + shirts
-				var inst = load(shirt).instance()
-				shirts_grid.add_child(inst)
-				inst.set_owner(self)
+		# Load the DLC
+		ProjectSettings.load_resource_pack(dlc_path, false)
+		
+		# Initialize and add nodes from each tab 
+		for scene in scenes:
+			var dlc_scene = "res://scenes/dlc/" + scene
+			var init = load(dlc_scene).instance()
+			
+			if "shirts" in scene:
+				shirts_grid.add_child(init)
+				print_debug("Initialized: " + dlc_scene)
 				
-#			var import_scene = load(shirts).instance()
-#			shirts_grid.add_child(import_scene)
-#	if dir.open("user://dlc") == OK:
-#		print_debug(OS.get_user_data_dir())
-#		dir.list_dir_begin()
-#		var load_pcks = ProjectSettings.load_resource_pack(dir.get_next(), false)
-#		if load_pcks == true:
-#			if dir.change_dir("res://scenes/dlc/shirts") == OK:
-#				var file_name = dir.get_next()
-#				print_debug("loaded:" + file_name)
-#				if dir.current_is_dir() == "shirts":
-#					var shirt_dlc = load(dir.get_next())
-#					shirts_grid.get_children(shirt_dlc)
+			if "pants" in scene:
+				pants_grid.add_child(init)
+				print_debug("Initialized: " + dlc_scene)
+
+func _ready():
+	get_dlc("testdlc.pck", ["shirts/TestShirts.tscn"])
 			
 func _on_removeAccessory_pressed():
 	character.accessory = blank_top

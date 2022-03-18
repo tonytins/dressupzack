3e9010e5e345498a234e9ce3c7a94edb1d7edbe3
Tony Bark <35226681+tonytins@users.noreply.github.com>
2022-03-18 14:08:33 -0400

    Initial port to GDevlop 5

--- scripts/clothing/undies_base.gd
+++
@@ -1,7 +0,0 @@
-# This project is licensed under the Artistic-2.0 license.
-# See the LICENSE file in the project root for more information.
-extends "res://scripts/clothing/clothing_base.gd"
-
-func _on_undies_pressed():
-	GameData.underwear = texture_normal
-	$select.play()
